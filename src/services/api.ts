1c04e4dce3da19d109fce311ac9e64a9dfceba16
Antonio Annes <antonio.vmachado13@gmail.com>
2023-08-08 14:37:53 -0300

    changing api

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,11 +4,11 @@ import { Game } from '../App'
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://fake-api-tau.vercel.app'
+    baseUrl: 'https://fake-api-tau.vercel.app/api/ebac_sports'
   }),
   endpoints: (builder) => ({
     getJogos: builder.query<Game[], void>({
-      query: () => 'produtos'
+      query: () => 'store'
     })
   })
 })
